7682f54c5455cb9e20ff89a209fac4ac8d9baa9e
Youssef Ben Attar <youssefdisvitas@gmail.com>
2023-08-16 00:37:18 +0100

    more stuffs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MainApp());
+void main() => runApp(const RingTonesList());
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -19,25 +19,28 @@ class MainApp extends StatelessWidget {
                 Container(
                   height: 300.0,
                   color: Colors.blue,
-                  child: const Card(
-                      color: Colors.transparent,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          CircleAvatar(
-                            radius: 45,
-                            backgroundImage: AssetImage('icons/1.jpg'),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            'Ringtones',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 30),
-                          )
-                        ],
-                      )),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: const Card(
+                        color: Colors.transparent,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircleAvatar(
+                              radius: 45,
+                              backgroundImage: AssetImage('icons/1.jpg'),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'Ringtones',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30),
+                            )
+                          ],
+                        )),
+                  ),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -149,6 +152,121 @@ class MainApp extends StatelessWidget {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class RingTonesList extends StatelessWidget {
+  const RingTonesList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: ListView(children: [
+          Container(
+            height: 65,
+            child: Card(
+              color: Colors.transparent,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('numbers/1.jpg'),
+                  ),
+                  const Text(
+                    'Ringtone_1',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                  Container(
+                      alignment: Alignment.centerRight,
+                      child: Icon(Icons.share))
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 65,
+            child: Card(
+              color: Colors.transparent,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('numbers/2.jpg'),
+                  ),
+                  const Text(
+                    'Ringtone_2',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                  Container(
+                      alignment: Alignment.centerRight,
+                      child: Icon(Icons.share))
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 65,
+            child: Card(
+              color: Colors.transparent,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('numbers/3.jpg'),
+                  ),
+                  const Text(
+                    'Ringtone_3',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                  Container(
+                      alignment: Alignment.centerRight,
+                      child: Icon(Icons.share))
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 65,
+            child: Card(
+              color: Colors.transparent,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('numbers/4.jpg'),
+                  ),
+                  const Text(
+                    'Ringtone_4',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15),
+                  ),
+                  Container(
+                      alignment: Alignment.centerRight,
+                      child: Icon(Icons.share))
+                ],
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
